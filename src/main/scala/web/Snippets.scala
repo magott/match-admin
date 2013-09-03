5cb86152a9c9c63652fd9e24d3f1e56de182de00
Morten Andersen-Gott <morten.andersengott@gmail.com>
2013-09-03 21:53:24 +0200

    Added language hint in meta data

--- src/main/scala/web/Snippets.scala
+++ src/main/scala/web/Snippets.scala
@@ -402,6 +402,7 @@ case class Snippets(req: HttpRequest[_]) {
         {meta}
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="description" content=""/>
+        <meta http-equiv="Content-Language" content="no" />
         <meta name="author" content="Morten Andersen-Gott"/>
 
         <!-- Le styles -->
