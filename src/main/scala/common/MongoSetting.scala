d3303f9b320bf6242789ff31dd48fe142a8eef01
Morten Andersen-Gott <morten.andersengott@gmail.com>
2020-10-16 11:03:18 +0200

    update mongo java driver - retry writes disabled

--- src/main/scala/common/MongoSetting.scala
+++ src/main/scala/common/MongoSetting.scala
@@ -1,6 +1,6 @@
 package common
 
-import com.mongodb.ServerAddress
+import com.mongodb.{MongoClientOptions, ServerAddress}
 import com.mongodb.casbah.{MongoClient, MongoConnection, MongoCredential, MongoDB}
 
 object MongoSetting {
@@ -8,9 +8,10 @@ object MongoSetting {
     val regex = """mongodb://(\w+):(\w+)@([\w|\.]+):(\d+)/(\w+)""".r
     url match {
       case Some(regex(u, p, host, port, dbName)) =>
-        val credential = MongoCredential.createCredential(u, dbName, p.toCharArray)
-        val address    = new ServerAddress(host, port.toInt)
-        val client     = MongoClient(address, credential :: Nil)
+        val mongoClientOptions = MongoClientOptions.builder().retryWrites(false).build()
+        val credential         = MongoCredential.createCredential(u, dbName, p.toCharArray)
+        val address            = new ServerAddress(host, port.toInt)
+        val client             = MongoClient(address, credential :: Nil, mongoClientOptions)
         Some(client.getDB(dbName))
       case None                                  =>
         Some(MongoClient("localhost", 27017).getDB("test"))
